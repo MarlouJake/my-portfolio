0feaf6570a7eed1d44c975951c8155d755ec978f
Jake <marlounapoli1305@gmail.com>
2024-11-22 18:13:51 +0800

    added dynamic paging

---
+++ controller/ViewController.js
@@ -0,0 +1,98 @@
+export default class ViewController{
+
+    constructor(url = '', container = '.index-main', data = {}){
+        this.controller = "/view";
+        this.file_type = ".html";
+        this.url = (this.controller + url + this.file_type);
+        this.container = container;
+        this.data = (Object.keys(data).length > 0)  ? data : { default: null };
+        this.params = {
+            url: this.url,
+            container: this.container,
+            data: (Object.keys(data).length > 0) ? this.data : this.defaultData,
+        };
+    }
+
+    Profile(url = '', container = '', data = {}) {
+
+        this.url = (url.trim() === '') ? '/view/home.html' : url;
+        this.container = (container.trim() === '') ?  this.container : container;
+        this.data =  (Object.keys(data).length > 0) ? data : this.data
+        this.params = {
+            url: this.url,
+            container: this.container,
+            data: this.data
+        };
+
+        return this;
+    }
+
+    About(url = '', container = '', data = {}) {
+
+        this.url = (url.trim() === '') ? '/view/about.html' : url;
+        this.container = (container.trim() === '') ?  this.container : container;
+
+        this.params = {
+            url: this.url,
+            element: this.container,
+            data: (Object.keys(data).length > 0) ? data : this.data,
+        };
+
+        return this;
+    }
+
+    Skills(url = '', container = '', data = {}) {
+
+        this.url = (url.trim() === '') ? '/view/skills.html' : url;
+        this.container = (container.trim() === '') ?  this.container : container;
+
+        this.params = {
+            url: this.url,
+            element: this.container,
+            data: (Object.keys(data).length > 0) ? data : this.data,
+        };
+
+        return this;
+    }
+
+    async LoadContent(url = '', container = this.container, data = this.data){
+
+        url = (url.trim() !== '') ? (this.controller + url + this.file_type) : this.url;
+
+        try{
+            const response = await $.ajax({
+                url:  url,
+                type: 'GET',
+                data: data,
+                cache: true,
+                timeout: 10000
+            });
+
+            $(container).html(response);
+            await this.updateActive(url);
+        }
+        catch (error){
+            this.params.url = url;
+            console.error(`[${error.status} ${error.statusText}] An error occurred while loading the view.
+                \n%cPayload:`, 'font-weight: bold;', `\n\t\t${JSON.stringify(this.params)}`);
+        }
+
+        return this.params;
+    };
+
+    async updateActive(url){
+        $('.index-nav .nav-ul li a').removeClass('active');
+
+        let uri = $('.index-nav .nav-ul li').find('a').filter(function(){
+            return $(this).data('uri') === url;
+        });
+
+        if(uri.length){
+            $(uri).addClass('active');
+        } else{
+            $(uri).removeClass('active');
+        }
+
+        
+    }
+}
---
+++ www/js/dynamic.js
@@ -0,0 +1,19 @@
+import ViewController from "../../controller/ViewController.js";
+
+$(document).ready(async function(){
+
+    let controller = new ViewController();
+
+    await controller.Profile().LoadContent();
+    await controller.updateActive('/home');
+
+    $('.index-nav .nav-ul li').on('click', async function(){
+        let uri = $(this).find('a').data('uri');
+
+        await controller.LoadContent(uri);
+        await controller.updateActive(uri);
+    });
+});
+
+
+
